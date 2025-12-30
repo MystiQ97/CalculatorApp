6d3227399919cf34b770b09e307e29cb04623749
MystiQ97 <amund@live.se>
2025-12-30 23:51:20 +0100

    Add files via upload
    Moving files from laptop to github

---
+++ stack.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next = next_node
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head is None
+
+    def push(self, data):
+        old_head = self.head
+        self.head = Node(data, old_head)
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            data = self.head.data
+            self.head = self.head.next
+            return data
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.head.data
+
+
+
---
+++ window.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+
+class Window:
+    def __init__(self):
+        self.root = Tk()
+
+
+window = Window()
